2cc22585111e9102f407072a24e4a74a2e6495a1
scriptsgnu <scriptsgnulinux@gmail.com>
2023-03-01 17:50:34 +0100

    corrección error en /etc/init.d/impresora.sh

--- deb/etc/init.d/impresora.sh
+++ deb/etc/init.d/impresora.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+### BEGIN INIT INFO
+# Provides: impresora
+# Required-Start:    $local_fs $syslog $remote_fs dbus
+# Required-Stop:     $local_fs $syslog $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Script para el mantenimiento de los inyectores de la impresora
+### END INIT INFO
+# 
+#
+# scriptsgnulinux <scriptsgnulinux@gmail.com>
+
+
+
 if [[ $# -eq 0 ]]
 then
 	echo "Modo de empleo: /etc/init.d/impresora.sh {start|stop}"
