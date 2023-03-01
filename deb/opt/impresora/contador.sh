279016f3d1cb20e24ee639b73cf91b0dfc2490cf
scriptsgnu <scriptsgnulinux@gmail.com>
2023-03-01 18:47:42 +0100

    actualización contador.sh

--- deb/opt/impresora/contador.sh
+++ deb/opt/impresora/contador.sh
@@ -5,6 +5,7 @@ FICHERO=contador
 impre=`cat config | sed -n '3p'`
 if [ "$impre" = "---" ]
 then
+	sleep 100
 	sed -i "3 s/^.*$/$(lpstat -a | cut -d ' ' -f1)/g" config
 	impre=`lpstat -a | cut -d ' ' -f1`
 fi
